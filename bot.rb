c3a856b197e4ecf359cd465eff8c2b8ed74d84be
Vladislav Ermolovich <vlad.ermaac@gmail.com>
2017-07-08 16:27:23 +0300

    fix tabs

--- bot.rb
+++ bot.rb
@@ -7,44 +7,44 @@ load_all LIB_PATH
 
 def content_for_gif? bot, message
   return MakeGifFromUrlCommand.new(bot, message, message.text) if message.text =~ %r{https?://.*\..*/?.*}
- 	return MakeGifFromFileCommand.new(bot, message) if message[:video]
- 	false
+  return MakeGifFromFileCommand.new(bot, message) if message[:video]
+  false
 end
 
 def get_general_command bot, message
-	command = message.text.downcase.to_sym if message.text
-	if COMMANDS.include? command
-		COMMANDS[command].new bot, message
-	else
+  command = message.text.downcase.to_sym if message.text
+  if COMMANDS.include? command
+    COMMANDS[command].new bot, message
+  else
     result = content_for_gif? bot, message
-		if result
-			result
-		else
-			UnknownCommand.new bot, message
-		end
-	end
+    if result
+      result
+    else
+      UnknownCommand.new bot, message
+    end
+  end
 end
 
 def make_gif_command bot, message
-	case message.data
-	when "Via URL" then AskForUrlCommand.new bot, message
-	when "From file on your device" then AskForFileCommand.new bot, message
-	end
+  case message.data
+  when "Via URL" then AskForUrlCommand.new bot, message
+  when "From file on your device" then AskForFileCommand.new bot, message
+  end
 end
 
 COMMANDS = {"/make_gif": ContentProvideWayCommand, "/start": Start, "/help": Help}
 def get_command bot, message
-	case message
-	when Telegram::Bot::Types::Message then get_general_command bot, message
-	when Telegram::Bot::Types::CallbackQuery then make_gif_command bot, message
-	end
+  case message
+  when Telegram::Bot::Types::Message then get_general_command bot, message
+  when Telegram::Bot::Types::CallbackQuery then make_gif_command bot, message
+  end
 end
 
 Telegram::Bot::Client.run(ENV["TOKEN"]) do |bot|
   bot.listen do |message|
-  	Thread.start do
-	    command = get_command bot, message
-	    command.exec
-	  end
+    Thread.start do
+      command = get_command bot, message
+      command.exec
+    end
   end
 end
--- lib/content_provide_way_command.rb
+++ lib/content_provide_way_command.rb
@@ -1,11 +1,11 @@
 require_relative 'command.rb'
 
 class ContentProvideWayCommand < Command
-	def exec
-		message = get_message @chat_id, "Specify the way you want to provide content for Gif"
-		keyboard = make_keyboard "From file on your device", "Via URL"
-		markup = make_markup keyboard
-		add_markup message, markup
-		send_message @bot, message
-	end
+  def exec
+    message = get_message @chat_id, "Specify the way you want to provide content for Gif"
+    keyboard = make_keyboard "From file on your device", "Via URL"
+    markup = make_markup keyboard
+    add_markup message, markup
+    send_message @bot, message
+  end
 end
