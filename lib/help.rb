fa1c562179a1c7574e5d11ce1c354711ad52038e
Vladislav Ermolovich <vlad.ermaac@gmail.com>
2017-07-16 12:26:00 +0300

    fix styling

--- lib/help.rb
+++ lib/help.rb
@@ -2,12 +2,12 @@ require_relative 'command.rb'
 
 class Help < Command
   def exec
-    message = <<~EOS
-    Here is the list of available commands:
-    /start - bot greeting
-    /help - list of available commands
-    /make_gif - make Gif from provided source file
-    EOS
+    message = <<~HELP
+      Here is the list of available commands:
+      /start - bot greeting
+      /help - list of available commands
+      /make_gif - make Gif from provided source file
+    HELP
     print_message @bot, @chat_id, message
   end
 end
